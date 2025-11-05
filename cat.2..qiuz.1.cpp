a02ca585cc92c91f8778cc72bf0c5679aabc920a
kariz-networks <gichuhi.26129@students.kyu.ac.ke>
2025-11-05 13:25:50 +0300

    Add files via upload

---
+++ cat.2..qiuz.1.cpp
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+/*
+Name: Joseph kariuki gichuhi
+Reg: ct100/G/26129/25
+Description: 2d array
+*/
+#include <stdio.h>
+
+int main() {
+    int scores[4][2] = {
+        {65, 92},
+        {84, 72},
+        {35, 70},
+        {59, 67}
+    };
+    
+    // Declaration 
+    int rows = 4;
+    int columns = 2;
+    int i, j;
+
+    printf("--- Array  (Scores) ---\n");
+
+    for(i = 0; i < rows; i++) {       
+        
+        for(j = 0; j < columns; j++) {  
+            printf("%d\t", scores[i][j]);
+        }
+        
+        printf("\n");  
+    } 
+    return 0;
+	
+	
+	
+	
+	
+	
+}
